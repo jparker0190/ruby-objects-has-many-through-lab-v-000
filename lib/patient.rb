42f1ce955f190d999b7aa940af6669dfa1237391
Joseph Parker <jparker010190@gmail.com>
2018-09-24 00:48:36 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,15 +1,17 @@
 class Patient
-attr_reader :name, :appointments
-@@all = []
-def initialize(name)
-  @name = name
-  @appointments = []
-end
-def new_appointment(appointment)
-  self.appointments << appointment
-  appointment.patient = self
-end
-def doctors
-  self.appointments.map {|t| t.doctor}
-end
+  attr_accessor :name, :appointments
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+
+  def doctors
+    @appointments.collect {|appointment| appointment.doctor}
+  end
 end
